1b9c659aae1e71c6b52f5c31bba87b45db8adf3c
keanh <keanh275@gmail.com>
2020-02-26 21:28:04 +0700

    add

---
+++ DemoArrayList/src/MyListTest.java
@@ -0,0 +1,18 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<String> list = new MyList<>();
+        list.add("America");
+        list.add("Canada");
+        list.add("Russia");
+        list.add("France");
+        list.add("Germany");
+        list.add("Norway");
+        //list.remove(2);
+        System.out.println(list);
+
+        //list.clear();
+        System.out.println(list.contains("Germany"));
+        System.out.println(list.get(3));
+        System.out.println(list.indexOf("America"));
+    }
+}
