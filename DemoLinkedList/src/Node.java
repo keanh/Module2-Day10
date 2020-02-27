fb208af2a79d6ce6b7e4fbbd4c340e6e01bd1ec7
keanh <keanh275@gmail.com>
2020-02-27 08:54:09 +0700

    add

---
+++ DemoLinkedList/src/Node.java
@@ -0,0 +1,15 @@
+class Node<E> {
+    E element; Node<E> next;
+    public Node(E e) {
+    element = e;
+    }
+
+    Node<String> head = null;
+    Node<String> tail = null;
+
+//    Node current = head;
+//    while(current != null){
+//        System.out.println(current.element);
+//        current = current.next();
+//    }
+}
